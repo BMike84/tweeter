87b5d95aef89a2cbdb42bc891274991582a685ba
Michael Buffone <m_buffone@hotmail.com>
2022-03-17 07:49:12 -0400

    This added a counter function to count down from 140 characters and turn red when under 0

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  // specifying the text area with #tweet-text
+ $('#tweet-text').on('input', function(e) {
+   // counts the number of inputs pressed
+   let charCount = $(this).val().length;
+   let remainChars = 140 - charCount;
+   // find the class counter in the html file
+   let counter = $(this).parent().find('.counter');
+   // updates the class counter text with remaining characters
+   counter.text(remainChars);
+   // adds a class to output tag to turn color red if the number remaining is less then 0
+   if (remainChars < 0) {
+     counter.addClass('negative-count');
+   }
+ });
+});
