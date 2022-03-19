248ad8589e1cf207fa3d79ddd28550a7486db358
Michael Buffone <m_buffone@hotmail.com>
2022-03-19 11:33:28 -0400

    This added the render function to load data of hard coded users

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,38 +4,60 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
-const tweetData = {
+const data = [
+  {
     "user": {
       "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
     "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
     "created_at": 1461116232227
- }
- 
- $(document).ready(function() {
-  const createTweetElement = function(tweetData) {
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+$(document).ready(function() {
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
+  }
+
+  const createTweetElement = function(tweet) {
     const $tweet = (`
         <article class="article-tweets">
           <header>
             <div class="user">
-            <img src="${tweetData.user.avatars}" alt="">
-              <p>${tweetData.user.name}</p>
+            <img src="${tweet.user.avatars}" alt="">
+              <p>${tweet.user.name}</p>
             </div>
-            <p>${tweetData.user.handle}</p>
+            <p>${tweet.user.handle}</p>
           </header>
           <div class="tweet">
-            <p>${tweetData.content.text}</p>
+            <p>${tweet.content.text}</p>
             <div class="border"></div>
           </div>
           <footer>
             <div class="left-footer">
-              <span>${tweetData.created_at}</span>
+              <span>${tweet.created_at}</span>
             </div>
             <div class="right-footer">
               <i class="fa-solid fa-flag"></i>
@@ -48,8 +70,6 @@ const tweetData = {
     return $tweet
   }
 
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  $('#tweets-container').append($tweet);
+  renderTweets(data);
 
 })
