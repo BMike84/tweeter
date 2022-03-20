75ed6b02d05a0f71931a05e972287477d026aa68
Michael Buffone <m_buffone@hotmail.com>
2022-03-20 16:23:11 -0400

    This added functionality for clicking submit and also added in load tweet function and removed hard coded data object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 $(document).ready(function() {
 
@@ -57,7 +57,7 @@ $(document).ready(function() {
           </div>
           <footer>
             <div class="left-footer">
-              <span>${tweet.created_at}</span>
+              <span>${timeago.format(tweet.created_at)}</span>
             </div>
             <div class="right-footer">
               <i class="fa-solid fa-flag"></i>
@@ -70,6 +70,31 @@ $(document).ready(function() {
     return $tweet
   }
 
-  renderTweets(data);
+  // renderTweets(data);
+
+// for submission
+ $("#form-tweet").submit(function(event) {
+  console.log('Event called');
+  event.preventDefault();
+  $.ajax("/tweets", { method: 'Post', data: $(this).serialize()});
+  console.log($(this).serialize())
+ });
+
+//  $("#tweet-btn").click(function() {
+//   $("#form-tweet").submit();
+//  });
+
+
+//load tweets
+const loadTweets = function () {
+  // using a get request to search the  page for all tweets
+  $.ajax("/tweets", { method: 'GET' })
+  .then(function (tweets) {
+    // console.log('Success: ', tweets);
+    renderTweets(tweets);
+  })
+}
+
+loadTweets();
 
 })
