98c789bab51e2276fc8202f07d0180d2007302de
Michael Buffone <m_buffone@hotmail.com>
2022-03-22 11:54:34 -0400

    This made the buttons clickable and displaying proper message

--- public/scripts/form-toggle.js
+++ public/scripts/form-toggle.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
+  // does not display compose tweet
   $('.new-tweet').hide()
-  $('.write-tweet').on('click', function() {
-    $('.new-tweet').slideToggle();
+  // when clicking first button second button appears
+  $('.nav-btn').on('click', function() {
+    $('.new-tweet').slideDown();
+    $('.nav-btn').addClass('hide');
+    $('.nav-btn2').removeClass('hide');
   });
+  //when clicking second button returns to first button
+  $('.nav-btn2').on('click', function() {
+    $('.new-tweet').slideUp();
+    $('.nav-btn2').addClass('hide');
+    $('.nav-btn').removeClass('hide');
+  })
 })
