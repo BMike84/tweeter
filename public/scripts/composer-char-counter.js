0b46bc1d787734cfd4bd17979eecc50467d03847
Michael Buffone <m_buffone@hotmail.com>
2022-03-22 08:22:30 -0400

    This used eslint

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
   // specifying the text area with #tweet-text
- $('#tweet-text').on('input', function(e) {
-   // counts the number of inputs pressed
-   let charCount = $(this).val().length;
-   let remainChars = 140 - charCount;
-   // find the class counter in the html file
-   let counter = $(this).parent().find('.counter');
-   // updates the class counter text with remaining characters
-   counter.text(remainChars);
-   // adds a class to output tag to turn color red if the number remaining is less then 0
-   if (remainChars < 0) {
-     counter.addClass('negative-count');
-   } else {
-     counter.removeClass('negative-count');
-   }
- });
+  $('#tweet-text').on('input', function() {
+    // counts the number of inputs pressed
+    let charCount = $(this).val().length;
+    let remainChars = 140 - charCount;
+    // find the class counter in the html file
+    let counter = $(this).parent().find('.counter');
+    // updates the class counter text with remaining characters
+    counter.text(remainChars);
+    // adds a class to output tag to turn color red if the number remaining is less then 0
+    if (remainChars < 0) {
+      counter.addClass('negative-count');
+    } else {
+      counter.removeClass('negative-count');
+    }
+  });
 });
