3910600e1430f234c1e8e85e1dc0f5e7a34ff731
Michael Buffone <m_buffone@hotmail.com>
2022-03-22 14:10:17 -0400

    This added scroll function for top of back button

--- public/scripts/form-toggle.js
+++ public/scripts/form-toggle.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // For Nav Bar
   // does not display compose tweet
   $('.new-tweet').hide()
   // when clicking first button second button appears
@@ -17,5 +18,20 @@ $(document).ready(function() {
     $('.new-tweet').slideUp();
     $('.nav-btn2').addClass('hide');
     $('.nav-btn').removeClass('hide');
-  })
+  });
+
+  // scroll page
+  $(document).scroll(function(){
+
+    const position = $(window).scrollTop()
+    if(position > 120){
+      $('.scroll-btn').removeClass('hide');
+      $('.scroll-btn').on('click', function(){
+        $(window).scrollTop(0);
+        $('.scroll-btn').addClass('hide');
+      });
+    } else {
+      $('.scroll-btn').addClass('hide');
+    }
+  });
 })
